7a279eae989aceea56b52fe4f5c305d3929979fb
Dave Kelsey <d_kelsey@uk.ibm.com>
2017-07-05 17:00:38 +0100

    changes to reflect new dev server configuration (#64)
    Signed-off-by: Dave Kelsey <d_kelsey@uk.ibm.com>

--- packages/vehicle-lifecycle/installers/hlfv1/build.sh
+++ packages/vehicle-lifecycle/installers/hlfv1/build.sh
@@ -5,8 +5,8 @@ cd "${DIR}"
 
 cat install.sh.in | sed 's/{{ENV}}//g' > install.sh
 echo "PAYLOAD:" >> install.sh
-tar czf - flows.json vehicle-lifecycle-network.bna docker-compose-playground.yml fabric-dev-servers mychannel.tx twoorgs.genesis.block >> install.sh
+tar czf - flows.json vehicle-lifecycle-network.bna docker-compose-playground.yml fabric-dev-servers >> install.sh
 
 cat install.sh.in | sed 's/{{ENV}}/-unstable/g' > install-unstable.sh
 echo "PAYLOAD:" >> install-unstable.sh
-tar czf - flows.json vehicle-lifecycle-network.bna docker-compose-playground-unstable.yml fabric-dev-servers mychannel.tx twoorgs.genesis.block >> install-unstable.sh
+tar czf - flows.json vehicle-lifecycle-network.bna docker-compose-playground-unstable.yml fabric-dev-servers >> install-unstable.sh
