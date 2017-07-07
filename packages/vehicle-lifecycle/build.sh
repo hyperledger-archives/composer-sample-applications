e1c8c7763e96e1fffdf4f05597a430d0f673f2e3
Simon Stone <sstone1@uk.ibm.com>
2017-07-07 15:56:21 +0100

    Remove Docker Compose usage, wait for apps to start (#67)

--- packages/vehicle-lifecycle/build.sh
+++ packages/vehicle-lifecycle/build.sh
@@ -7,18 +7,18 @@ set -ev
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}/installers"
 
+# clean up
+rm -rf $VERSION/fabric-dev-servers/
+rm -f fabric-dev-servers.zip
 
 # Get the fabric tools
 curl -O https://raw.githubusercontent.com/hyperledger/composer-tools/master/packages/fabric-dev-servers/fabric-dev-servers.zip
 
 # Build all of the installers.
-
-
 VERSION=hlfv1
 unzip -q fabric-dev-servers.zip -d $VERSION/fabric-dev-servers/
 $VERSION/build.sh
 
-rm -rf $VERSION/fabric-dev-servers/
-
 # clean up
-rm fabric-dev-servers.zip
+rm -rf $VERSION/fabric-dev-servers/
+rm -f fabric-dev-servers.zip
--- packages/vehicle-lifecycle/installers/hlfv1/build.sh
+++ packages/vehicle-lifecycle/installers/hlfv1/build.sh
@@ -3,10 +3,10 @@ set -ev
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}"
 
-cat install.sh.in | sed 's/{{ENV}}//g' > install.sh
+cat install.sh.in | sed 's/{{ENV}}/latest/g' > install.sh
 echo "PAYLOAD:" >> install.sh
-tar czf - flows.json vehicle-lifecycle-network.bna docker-compose-playground.yml fabric-dev-servers >> install.sh
+tar czf - flows.json vehicle-lifecycle-network.bna fabric-dev-servers >> install.sh
 
-cat install.sh.in | sed 's/{{ENV}}/-unstable/g' > install-unstable.sh
+cat install.sh.in | sed 's/{{ENV}}/unstable/g' > install-unstable.sh
 echo "PAYLOAD:" >> install-unstable.sh
-tar czf - flows.json vehicle-lifecycle-network.bna docker-compose-playground-unstable.yml fabric-dev-servers >> install-unstable.sh
+tar czf - flows.json vehicle-lifecycle-network.bna fabric-dev-servers >> install-unstable.sh
