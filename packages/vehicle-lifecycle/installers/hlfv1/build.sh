8635a8bd06bd2fdc10a2bf8a2abb242eb034a0bb
Simon Stone <sstone1@uk.ibm.com>
2017-09-01 10:33:03 +0100

    Stop renaming to org-acme-biznet, use ID cards (#85)

--- packages/vehicle-lifecycle/installers/hlfv1/build.sh
+++ packages/vehicle-lifecycle/installers/hlfv1/build.sh
@@ -7,31 +7,15 @@ ROOT=$DIR/../..
 cd $ROOT
 npm install
 
-mkdir -p $ROOT/tmp
-cd $ROOT/tmp
-
-unzip $ROOT/node_modules/vehicle-lifecycle-network/dist/vehicle-lifecycle-network.bna
-sed -i.bak 's/vehicle-lifecycle-network/org-acme-biznet/g' package.json
-zip -r vehicle-lifecycle-network.bna *
-
 cd "${DIR}"
 cat install.sh.in | sed 's/{{ENV}}/latest/g' > install.sh
 echo "PAYLOAD:" >> install.sh
-tar czf - -C $ROOT/tmp vehicle-lifecycle-network.bna -C $DIR flows.json fabric-dev-servers >> install.sh
-
-rm -rf $ROOT/tmp/*
+tar czf - -C $ROOT/node_modules/vehicle-lifecycle-network/dist vehicle-lifecycle-network.bna -C $DIR flows.json fabric-dev-servers >> install.sh
 
 cd $ROOT
 npm install vehicle-lifecycle-network@unstable
 
-cd $ROOT/tmp
-unzip $ROOT/node_modules/vehicle-lifecycle-network/dist/vehicle-lifecycle-network.bna
-sed -i.bak 's/vehicle-lifecycle-network/org-acme-biznet/g' package.json
-zip -r vehicle-lifecycle-network.bna *
-
 cd "${DIR}"
 cat install.sh.in | sed 's/{{ENV}}/unstable/g' > install-unstable.sh
 echo "PAYLOAD:" >> install-unstable.sh
-
-tar czf - -C $ROOT/tmp vehicle-lifecycle-network.bna -C $DIR flows.json fabric-dev-servers >> install-unstable.sh
-rm -rf $ROOT/tmp
+tar czf - -C $ROOT/node_modules/vehicle-lifecycle-network/dist vehicle-lifecycle-network.bna -C $DIR flows.json fabric-dev-servers >> install-unstable.sh
