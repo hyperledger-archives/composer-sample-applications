d7e347621ff5821d848d3d058848f92a106b4b09
Simon Stone <sstone1@uk.ibm.com>
2018-03-29 09:36:39 +0100

    Update incorrect ISC licenses (#125)
    * Update incorrect ISC licenses
    
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>
    
    * Fix digitalproperty-app, broken by 0.19.0
    
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>

---
+++ packages/digitalproperty-app/updateNetwork.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -ev
+VERSION=$(node -e 'console.log(require("digitalproperty-network/package.json").version)')
+composer archive create --sourceName digitalproperty-network --sourceType module --archiveFile digitalPropertyNetwork.bna
+composer network install --archiveFile ./digitalPropertyNetwork.bna --card PeerAdmin@hlfv1
+composer network upgrade --networkName digitalproperty-network --networkVersion ${VERSION} --card PeerAdmin@hlfv1
+composer network list --card admin@digitalproperty-network
