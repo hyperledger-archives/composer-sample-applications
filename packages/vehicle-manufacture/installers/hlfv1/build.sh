515b257102c59d4cd560a7e21afd656c8786e81e
Andrew Hurt <31732124+awjh-ibm@users.noreply.github.com>
2018-05-30 14:48:50 +0100

    uppdated for v0.19.x composer (#135)
    Signed-off-by: awjh-ibm <andrew.hurt1@ibm.com>

--- packages/vehicle-manufacture/installers/hlfv1/build.sh
+++ packages/vehicle-manufacture/installers/hlfv1/build.sh
@@ -18,11 +18,14 @@ ROOT=$DIR/../..
 cd $ROOT
 npm install
 
+NETWORK_VERSION=$(grep -o '"version": *"[^"]*"' $ROOT/node_modules/vehicle-manufacture-network/package.json | grep -o '[0-9]\.[0-9]\.[0-9]')
+
 cd "${DIR}"
 cat install.sh.in | sed \
     -e 's/{{COMPOSER-VERSION}}/latest/g' \
     -e 's/{{VEHICLE-MANUFACTURE-VERSION}}/latest/g' \
     -e 's/{{NODE-RED-VERSION}}/latest/g' \
+    -e "s/{{NETWORK-VERSION}}/$NETWORK_VERSION/g" \
     > install.sh
 echo "PAYLOAD:" >> install.sh
 tar czf - -C $ROOT/node_modules/vehicle-manufacture-network/dist vehicle-manufacture-network.bna -C $DIR flows.json fabric-dev-servers >> install.sh
@@ -30,11 +33,14 @@ tar czf - -C $ROOT/node_modules/vehicle-manufacture-network/dist vehicle-manufac
 cd $ROOT
 npm install vehicle-manufacture-network@unstable --no-save
 
+UNSTABLE_NETWORK_VERSION=$(grep -o '"version": *"[^"]*"' $ROOT/node_modules/vehicle-manufacture-network/package.json | grep -o '[0-9]\.[0-9]\.[0-9]')
+
 cd "${DIR}"
 cat install.sh.in | sed \
     -e 's/{{COMPOSER-VERSION}}/unstable/g' \
     -e 's/{{VEHICLE-MANUFACTURE-VERSION}}/unstable/g' \
     -e 's/{{NODE-RED-VERSION}}/unstable/g' \
+    -e "s/{{NETWORK-VERSION}}/$UNSTABLE_NETWORK_VERSION/g" \
     > install-unstable.sh
 echo "PAYLOAD:" >> install-unstable.sh
 tar czf - -C $ROOT/node_modules/vehicle-manufacture-network/dist vehicle-manufacture-network.bna -C $DIR flows.json fabric-dev-servers >> install-unstable.sh
